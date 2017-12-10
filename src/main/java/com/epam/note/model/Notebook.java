b0578d8c59ba064a9116d456bf1ce7b250e90411
Arina Umushkina <Arina_Umushkina@epam.com>
2017-12-10 03:34:23 +0300

    add services for users

---
+++ src/main/java/com/epam/note/model/Notebook.java
@@ -0,0 +1,51 @@
+package com.epam.note.model;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Component
+@Entity
+@Table(name = "notebook")
+public class Notebook {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Notebook notebook = (Notebook) o;
+
+        if (id != notebook.id) return false;
+        return title != null ? title.equals(notebook.title) : notebook.title == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+}
