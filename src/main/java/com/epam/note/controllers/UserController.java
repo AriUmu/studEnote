10b85ac8e2d5d878099cb4540d8a35917e8705c2
Arina Umushkina <Arina_Umushkina@epam.com>
2017-12-21 13:32:45 +0300

    18.12.2017 add controllers

--- src/main/java/com/epam/note/controllers/UserController.java
+++ src/main/java/com/epam/note/controllers/UserController.java
@@ -3,17 +3,14 @@ package com.epam.note.controllers;
 import com.epam.note.model.User;
 import com.epam.note.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import static org.springframework.http.HttpStatus.*;
 
-@RequestMapping("${api.root}/user")
-@CrossOrigin(origins = "${origins}")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
@@ -22,6 +19,12 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<User> createUser(@RequestBody User user) throws Exception {
         return new ResponseEntity<User>(userService.saveUser(user), OK);
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public boolean getUser(@RequestBody User user) throws Exception {
+        return (userService.accessUserPage(user));
     }
 
 
